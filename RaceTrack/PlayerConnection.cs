65a01b0fb26f2a2833a13deb5aed704b63080286
Simen Haugerud Granlund <simen@hgranlund.com>
2018-08-08 16:55:48 +0200

    Forbedret socket kommunikasjonen og trådhåndtering

--- RaceTrack/PlayerConnection.cs
+++ RaceTrack/PlayerConnection.cs
@@ -7,82 +7,45 @@ using System.Threading;
 namespace RaceTrack {
     internal class PlayerConnection : IDisposable {
         public class StateObject {
-            // Client  socket.  
             public Socket workSocket = null;
-            // Size of receive buffer.  
             public const int BufferSize = 1024;
-            // Receive buffer.  
             public byte[] buffer = new byte[BufferSize];
-            // Received data string.  
             public StringBuilder sb = new StringBuilder ();
         }
         private char unitSeperatorChar = (char) Convert.ToInt32 ("0x1f", 16);
-        private (int speed, Socket update, Socket speedUpdate) _playerA;
+        private (int speed, Socket updateStreamer, Socket speedUpdate) _playerA;
         public PlayerConnection () {
             StartListening ();
+            updateStreamerConected.WaitOne();
         }
-        public ManualResetEvent allDone = new ManualResetEvent (false);
+        public ManualResetEvent updateStreamerConected = new ManualResetEvent (false);
 
         public int GetLatestSpeed () {
             return _playerA.speed;
         }
         public void StartListening () {
-            // Establish the local endpoint for the socket.  
+            updateStreamerConected.Reset ();
             // The DNS name of the computer  
-            // running the listener is "host.contoso.com".  
             IPHostEntry ipHostInfo = Dns.GetHostEntry (Dns.GetHostName ());
             IPAddress ipAddress = ipHostInfo.AddressList[0];
             IPEndPoint localEndPoint = new IPEndPoint (ipAddress, 11000);
-            IPEndPoint localEndPoint2 = new IPEndPoint (ipAddress, 11001);
+            Socket listener = new Socket (ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
 
-            // Create a TCP/IP socket.  
-            Socket listener = new Socket (ipAddress.AddressFamily,
-                SocketType.Stream, ProtocolType.Tcp);
-            // Create a TCP/IP socket.  
-            Socket listener2 = new Socket (ipAddress.AddressFamily,
-                SocketType.Stream, ProtocolType.Tcp);
-
-            // Bind the socket to the local endpoint and listen for incoming connections.  
             try {
-
                 listener.Bind (localEndPoint);
-                listener.Listen (2);
-
-                listener2.Bind (localEndPoint2);
-                listener2.Listen (2);
-
-                // while (true) {
-                    // Set the event to nonsignaled state.  
-                    allDone.Reset ();
-
-                    Console.WriteLine ("Waiting for a connection...");
-                    listener.BeginAccept (
-                        new AsyncCallback (AcceptCallback),
-                        listener);
-                    // Start an asynchronous socket to listen for connections.  
-                    listener2.BeginAccept (
-                        new AsyncCallback (AcceptCallback),
-                        listener2);
-
-                    // Wait until a connection is made before continuing.  
-                    // }
-                    allDone.WaitOne ();
-                    allDone.WaitOne ();
-                // }
+                listener.Listen (4);
+                listener.BeginAccept (new AsyncCallback (AcceptCallback), listener);
             } catch (Exception e) {
                 Console.WriteLine (e.ToString ());
+                throw;
             }
         }
 
         public void AcceptCallback (IAsyncResult ar) {
-            // Signal the main thread to continue.  
-            allDone.Set ();
-
-            // Get the socket that handles the client request.  
             Socket listener = (Socket) ar.AsyncState;
             Socket handler = listener.EndAccept (ar);
+            listener.BeginAccept (new AsyncCallback (AcceptCallback), listener);
 
-            // Create the state object.  
             StateObject state = new StateObject ();
             state.workSocket = handler;
             handler.BeginReceive (state.buffer, 0, StateObject.BufferSize, 0,
@@ -91,17 +54,12 @@ namespace RaceTrack {
 
         public void ReadCallback (IAsyncResult ar) {
             String content = String.Empty;
-
-            // Retrieve the state object and the handler socket  
-            // from the asynchronous state object.  
             StateObject state = (StateObject) ar.AsyncState;
             Socket handler = state.workSocket;
 
-            // Read data from the client socket.   
             int bytesRead = handler.EndReceive (ar);
 
             if (bytesRead > 0) {
-                // There  might be more data, so store the data received so far.  
                 state.sb.Append (Encoding.ASCII.GetString (
                     state.buffer, 0, bytesRead));
 
@@ -109,19 +67,16 @@ namespace RaceTrack {
                 // more data.  
                 content = state.sb.ToString ();
                 if (content.IndexOf (unitSeperatorChar) > -1) {
-                    // All the data has been read from the   
-                    // client. Display it on the console.  
                     string data = content.Replace (unitSeperatorChar.ToString (), "");
                     Console.WriteLine ("Data : {1}",
                         content.Length, data);
                     state.sb.Clear ();
-                    if (data == "PlayerA:UpdateMe") {
-                        _playerA = (speed: 0, update: handler, speedUpdate: null);
-                        SendTrackUpdates (handler);
+                    if (data == "TackUpdateStreamer") {
+                        _playerA.updateStreamer= handler;
+                        updateStreamerConected.Set ();
                         return;
-                    } else if (data == "PlayerA:Speed") {
-                        _playerA = (speed: 0, update: handler, speedUpdate: handler);
-                        return;
+                    } else if (data == "SpeedStreamer") {
+                        _playerA.speedUpdate= handler;
                     } else {
                         int speed = 0;
                         if (Int32.TryParse (data, out speed)) {
@@ -134,30 +89,29 @@ namespace RaceTrack {
             }
         }
 
-        private void SendTrackUpdates (Socket handler) {
-            var i = 1;
-            while (handler.Connected) {
-                i++;
-                Send (handler, "AnUpdate: " + i);
-                Thread.Sleep (1000);
+        public void SendTrackUpdate (string trackState) {
+            var socket = _playerA.updateStreamer;
+            if (socket.Connected) {
+                Send (socket, trackState);
+            } else {
+                Console.WriteLine ("Vi har mistet kontakt med trackStreamer socketen");
             }
         }
 
         private void Send (Socket handler, String data) {
-            // Convert the string data to byte data using ASCII encoding.  
-            byte[] byteData = Encoding.ASCII.GetBytes (data);
+            try {
+                byte[] byteData = Encoding.ASCII.GetBytes (data);
+                handler.BeginSend (byteData, 0, byteData.Length, 0,
+                    new AsyncCallback (SendCallback), handler);
 
-            // Begin sending the data to the remote device.  
-            handler.BeginSend (byteData, 0, byteData.Length, 0,
-                new AsyncCallback (SendCallback), handler);
+            } catch (System.Exception) {
+                CloseHandle (handler);
+            }
         }
 
         private void SendCallback (IAsyncResult ar) {
             try {
-                // Retrieve the socket from the state object.  
                 Socket handler = (Socket) ar.AsyncState;
-
-                // Complete sending the data to the remote device.  
                 int bytesSent = handler.EndSend (ar);
                 Console.WriteLine ("Sent {0} bytes to client.", bytesSent);
 
@@ -168,12 +122,16 @@ namespace RaceTrack {
 
         public void Dispose () {
             CloseHandle (_playerA.speedUpdate);
-            CloseHandle (_playerA.update);
+            CloseHandle (_playerA.updateStreamer);
         }
 
-        private void CloseHandle (Socket handle) {
-            handle.Shutdown (SocketShutdown.Both);
-            handle.Close ();
+        private void CloseHandle (Socket socket) {
+            if (socket == null) return;
+
+            if (socket.Connected) {
+                socket.Shutdown (SocketShutdown.Both);
+            }
+            socket.Close ();
         }
     }
 }
--- RaceTrack/Program.cs
+++ RaceTrack/Program.cs
@@ -6,26 +6,21 @@ using System.Threading;
 
 namespace RaceTrack {
 
-
     public class Program {
-        // State object for reading client data asynchronously  
-
-        // Thread signal.  
-       
+        
         public static int Main (String[] args) {
-            var track = new TrackConnection();
-            var player = new PlayerConnection();
-            
-            var speed = -1;
-            while (true)
-            {
-                if (speed != player.GetLatestSpeed())
-                {
-                    speed= player.GetLatestSpeed();
-                    track.SetSpeed(speed);
+            //var track = new TrackConnection();
+            using (var player = new PlayerConnection ()) {
+                var speed = -1;
+                while (true) {
+                    if (speed != player.GetLatestSpeed ()) {
+                        speed = player.GetLatestSpeed ();
+                        //track.SetSpeed(speed);
+                    }
+                    player.SendTrackUpdate ("En track update: " + DateTime.Now.Ticks);
+                    Thread.Sleep (1000);
                 }
-                Thread.Sleep(1000);
-            }
+            };
             return 0;
         }
     }
